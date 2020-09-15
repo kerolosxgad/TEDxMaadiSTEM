28208f2bfa5125f663fddd261603cf559476b29c
FadyJohn10 <63600279+FadyJohn10@users.noreply.github.com>
2020-09-16 00:28:48 +0200

    عبدو انت لازم تطور نفسك ريسبنوسف

--- js/main.js
+++ js/main.js
@@ -34,14 +34,46 @@ function myFunction() {
   
     if (dots.style.display === "none") {
       dots.style.display = "inline";
-      btnText.innerHTML = "Read more"; 
+      btnText.innerHTML = "Read full article"; 
       moreText.style.display = "none";
     } else {
       dots.style.display = "none";
       btnText.innerHTML = "Read less"; 
       moreText.style.display = "inline";
     }
-  }
+}
+
+function myFunction2() {
+    var dots = document.getElementById("dots2");
+    var moreText = document.getElementById("read-more-target2");
+    var btnText = document.getElementById("myBtn2");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read full article"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+}
+
+function myFunction3() {
+    var dots = document.getElementById("dots3");
+    var moreText = document.getElementById("read-more-target3");
+    var btnText = document.getElementById("myBtn3");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read full article"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+}
 
 /* global $, window */
 
