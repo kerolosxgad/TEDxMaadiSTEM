a157a1291fb8496bed6b6c38f35df03049a6ce41
FadyJohn10 <63600279+FadyJohn10@users.noreply.github.com>
2020-09-26 01:00:50 +0200

    اخيرااا

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ document.getElementById("demo").innerHTML = d.getUTCFullYear();
 
 //TEDx logo on small devices
 if (window.matchMedia("(max-width: 1199px)").matches) {
-    document.getElementById("navbar-logo").src = "../images/TEDxLogo.png";
+    document.getElementById("navbar-logo").src = "images/TEDxLogo.png";
 } else {
     //navbar background style on scroll
     window.onscroll = function myFunction() {
@@ -17,10 +17,10 @@ if (window.matchMedia("(max-width: 1199px)").matches) {
         var navh = document.getElementById("navbar").offsetHeight;
 
         if (document.body.scrollTop > navh || document.documentElement.scrollTop > navh) {
-            document.getElementById("navbar-logo").src = "../images/TEDxLogo.png";
+            document.getElementById("navbar-logo").src = "images/TEDxLogo.png";
             document.getElementById("navbar").classList.add("scrolled");
         } else {
-            document.getElementById("navbar-logo").src = "../images/TEDxLogoLight.png";
+            document.getElementById("navbar-logo").src = "images/TEDxLogoLight.png";
             document.getElementById("navbar").classList.remove("scrolled");
         }
 
